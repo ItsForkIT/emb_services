ad395c0e102c2d951932cba671d4fba023c9271f
Hridoy Dutta <hridaydutta123@gmail.com>
2017-06-16 23:59:39 +0530

    Initial Commit

---
+++ appGIS.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request
+from flask import request, render_template
+from flask import Flask
+import os, urllib, csv
+
+app = Flask (__name__)
+
+@app.route('/')
+def table():
+	file_url = 'https://gist.githubusercontent.com/hridaydutta123/f666b6990d5b00c2a862445230e4816c/raw/ef4d334845ebeb3d77cc09bfe87c65160df8b738/gistfile1.txt'
+	
+	response = urllib.urlopen(file_url)
+	emb_contents = list(csv.reader(response))
+	events = len(emb_contents)
+    
+	minTime = emb_contents[0][1]
+	maxTime = emb_contents[events-1][1]
+	print minTime
+	print maxTime
+
+	return render_template('table.html', events=events, data_records=emb_contents)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
