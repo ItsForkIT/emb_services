2278598e2db7b3c5f8ab160fd42bb8a336dcbd04
apex1000 <siddhartsingh9876@gmail.com>
2017-06-30 16:53:06 +0530

    graph

--- embservices.py
+++ embservices.py
@@ -10,5 +10,7 @@ def table():
 	result = firebase.get('/scores', None)
   	return render_template('index.html', messages=result)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, host='0.0.0.0')
