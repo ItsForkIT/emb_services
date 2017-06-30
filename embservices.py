43bd5995e8aeb24c93b5185bdfdce826ebaf8ff1
apex1000 <siddhartsingh9876@gmail.com>
2017-06-30 09:23:24 +0530

    real data

--- embservices.py
+++ embservices.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from firebase import firebase
 from flask import request
 from flask import request, render_template
 app = Flask (__name__)
 
+firebase = firebase.FirebaseApplication('https://haha-4cf04.firebaseio.com', None)
 @app.route('/')
 def table():
-	return render_template('index.html')
+	result = firebase.get('/scores', None)
+  	return render_template('index.html', messages=result)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
